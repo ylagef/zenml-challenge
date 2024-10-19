6558c2b333b3dae3821cdddbce5947b2413d811d
Yeray Lage <ylagef@gmail.com>
2024-10-19 11:59:55 +0200

    chore: add prettier and eslint configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,20 @@
-import type { Metadata } from "next";
-import { ReactNode } from "react";
-import "./globals.css";
-
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "ZenML Challenge - Yeray",
-  description: "Coding challenge for the Senior Frontend developer role at ZenML",
-};
+  title: 'ZenML Challenge - Yeray',
+  description: 'Coding challenge for the Senior Frontend developer role at ZenML'
+}
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: ReactNode;
+  children: ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className="antialiased"
-      >
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
-  );
+  )
 }
